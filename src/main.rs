919429aca90997b0100f965d105b1f1400830b6d
Zach Petch <zachpetch@gmail.com>
2023-04-17 14:23:43 -0700

    Adding math error enum for potential future use

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+/*
+ * This is stolen from noboilerplate on youtube, but seems useful for these purposes.
+ */
+#[derive (Debug)]
+enum MathError {
+    DivisionByZero,
+    NonPositiveLogarithm,
+    NegativeSquareRoot,
+}
+
 fn main() {
     println!("Hello, world!");
 }
+
